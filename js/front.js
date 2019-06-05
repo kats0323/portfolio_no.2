f5cb93392a612b292c5be8943f8e9aeb5ec88576
Kats <katsu.0323.jpn@icloud.com>
2019-06-05 21:42:18 +1000

    change

--- js/front.js
+++ js/front.js
@@ -1,25 +1,25 @@
 particlesJS('hoge',{
   "particles":{
  
-//--シェイプの設定----------
+
       "number":{
-        "value":80, //シェイプの数
+        "value":80, 
         "density":{
-          "enable":true, //シェイプの密集度を変更するか否か
-          "value_area":200 //シェイプの密集度
+          "enable":true, 
+          "value_area":200 
         }
       },
       "shape":{
-        "type":"circle", //シェイプの形（circle:丸、edge:四角、triangle:三角、polygon:多角形、star:星型、image:画像）
+        "type":"circle", 
         "stroke":{
-          "width":0, //シェイプの外線の太さ
-          "color":"#ffcc00" //シェイプの外線の色
+          "width":0, 
+          "color":"#ffcc00" 
         },
-        //typeをpolygonにした時の設定
+        
         "polygon": {
-          "nb_sides": 5 //多角形の角の数
+          "nb_sides": 5 
         },
-        //typeをimageにした時の設定
+        
         "image": {
           "src": "images/hoge.png",
           "width": 100,
@@ -27,48 +27,48 @@ particlesJS('hoge',{
         }
       },
       "color":{
-        "value":"#004d25" //シェイプの色
+        "value":"#004d25" 
       },
       "opacity":{
-        "value":1, //シェイプの透明度
-        "random":false, //シェイプの透明度をランダムにするか否か
+        "value":1, 
+        "random":false, 
         "anim":{
-          "enable":false, //シェイプの透明度をアニメーションさせるか否か
-          "speed":100, //アニメーションのスピード
-          "opacity_min":0.1, //透明度の最小値
-          "sync":false //全てのシェイプを同時にアニメーションさせるか否か
+          "enable":false, 
+          "speed":100, 
+          "opacity_min":0.1, 
+          "sync":false 
         }
       },
       "size":{
-        "value":2, //シェイプの大きさ
-        "random":true, //シェイプの大きさをランダムにするか否か
+        "value":2, 
+        "random":true, 
         "anim":{
-          "enable":false, //シェイプの大きさをアニメーションさせるか否か
-          "speed":40, //アニメーションのスピード
-          "size_min":0.1, //大きさの最小値
-          "sync":false //全てのシェイプを同時にアニメーションさせるか否か
+          "enable":false, 
+          "speed":40, 
+          "size_min":0.1, 
+          "sync":false 
         }
       },
-//--------------------
 
-//--線の設定----------
+
+
       "line_linked":{
-        "enable":true, //線を表示するか否か
-        "distance":150, //線をつなぐシェイプの間隔
-        "color":"#00a968", //線の色
-        "opacity":0.4, //線の透明度
-        "width":1 //線の太さ
+        "enable":true, 
+        "distance":150, 
+        "color":"#00a968", 
+        "opacity":0.4, 
+        "width":1 
       },
-//--------------------
 
-//--動きの設定----------
+
+
       "move":{
-        "speed":5, //シェイプの動くスピード
-        "straight":false, //個々のシェイプの動きを止めるか否か
-        "direction":"none", //エリア全体の動き(none、top、top-right、right、bottom-right、bottom、bottom-left、left、top-leftより選択)
-        "out_mode":"out" //エリア外に出たシェイプの動き(out、bounceより選択)
+        "speed":5, 
+        "straight":false, 
+        "direction":"none", 
+        "out_mode":"out" 
       }
-//--------------------
+
 
     },
  
@@ -76,65 +76,43 @@ particlesJS('hoge',{
       "detect_on":"canvas",
       "events":{
 
-//--マウスオーバー時の処理----------
-        "onhover":{
-          "enable":true, //マウスオーバーが有効か否か
-          "mode":"repulse" //マウスオーバー時に発動する動き(下記modes内のgrab、repulse、bubbleより選択)
-        },
-//--------------------
 
-//--クリック時の処理----------
-        "onclick":{
-          "enable":true, //クリックが有効か否か
-          "mode":"push" //クリック時に発動する動き(下記modes内のgrab、repulse、bubble、push、removeより選択)
+        "onhover":{
+          "enable":true, 
+          "mode":"repulse" 
         },
-//--------------------
+
+
+
+        "onclick":{
+          "enable":true, 
+          "mode":"push" 
+        },
+
 
       },
  
       "modes":{
 
-//--カーソルとシェイプの間に線が表示される----------
+
         "grab":{
-          "distance":10, //カーソルからの反応距離
+          "distance":10, 
           "line_linked":{
-            "opacity":1 //線の透明度
+            "opacity":1 
           }
         },
-//--------------------
 
-//--シェイプがカーソルから逃げる----------
+
+
         "repulse":{
-          "distance":40 //カーソルからの反応距離
+          "distance":40 
         },
-//--------------------
 
-//--シェイプが膨らむ----------
-        // "bubble":{
-        //   "distance":400, //カーソルからの反応距離
-        //   "size":40, //シェイプの膨らむ大きさ
-        //   "opacity":8, //膨らむシェイプの透明度
-        //   "duration":2, //膨らむシェイプの持続時間(onclick時のみ)
-        //   "speed":3 //膨らむシェイプの速度(onclick時のみ)
-        // },
-//--------------------
-
-//--シェイプが増える----------
-        // "push":{
-        //   "particles_nb":4 //増えるシェイプの数
-        // },
-//--------------------
-
-//--シェイプが減る----------
-        // "remove":{
-        //   "particles_nb":2 //減るシェイプの数
-        // }
-//--------------------
 
       }
     },
-    "retina_detect":true, //Retina Displayを対応するか否か
-    "resize":true //canvasのサイズ変更にわせて拡大縮小するか否か
+    "retina_detect":true, 
+    "resize":true 
   }
 );
 
